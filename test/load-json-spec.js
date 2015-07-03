088f959f88922df1926fafd73ede403a34c0adea
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 10:21:37 +0200

    Make test pass.

--- test/load-json-spec.js
+++ test/load-json-spec.js
@@ -10,3 +10,23 @@ describe('load ES6 kata data', function() {
     loadFileOnServer(katasUrl, onLoaded);
   });
 });
+
+
+var http = require('http');
+var url = require('url');
+var path = require('path');
+
+function loadFileOnServer(fileUrl, onLoaded) {
+  var data = '';
+  var options = url.parse(fileUrl);
+  options.headers = {'User-Agent': ''}; // github wants a user agent header
+  var request = http.request(options, function(res) {
+    res.on('data', function(chunk) {data += chunk;});
+    res.on('end', function() {
+      const parsed = JSON.parse(data);
+      onLoaded(null, parsed);
+    })
+  });
+  request.on('error', function(e) { onLoaded(e); });
+  request.end();
+}
