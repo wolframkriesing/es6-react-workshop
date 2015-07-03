f20ef3de15d38a0b2f32022149aa4f3689fcf39f
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 15:20:26 +0200

    Inject the http-get dependency.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import GroupedKata from './grouped-kata.js';
 import {KATAS_URL} from './config.js';
+import {loadFileOnServer} from './server/http-get.js';
 
-new GroupedKata(KATAS_URL).load(() => {}, (katas) => {
+new GroupedKata(loadFileOnServer, KATAS_URL).load(() => {}, (katas) => {
   document.getElementById('app').innerHTML = JSON.stringify(katas);
 });
