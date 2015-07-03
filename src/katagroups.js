9404b1da750d466e6beb624c7a67a1a20166a446
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 19:40:31 +0200

    Allow selecting a group by name.

--- src/katagroups.js
+++ src/katagroups.js
@@ -4,6 +4,16 @@ export default class KataGroups {
     kataGroups.groups = Object.keys(rawGroups).map(groupName => new KataGroup(groupName, rawGroups[groupName].items));
     return kataGroups;
   }
+  
+  selectGroupByName(nameToSelect) {
+    this.selectedGroup = null;
+    this.groups.some((group) => {
+      if (group.name === nameToSelect) {
+        this.selectedGroup = group;
+        return true; // stop iterating
+      }
+    });
+  }
 }
 
 class KataGroup {
--- test/katagroups-spec.js
+++ test/katagroups-spec.js
@@ -10,16 +10,16 @@ describe('kata data for rendering', function() {
     let firstGroup;
     beforeEach(function() {
       const rawData = {[groupName]: {items: [{name: 'jojo', path: 'some'}]}};
-      kataGroups = KataGroups.fromRawKataData(rawData).groups;
-      firstGroup = kataGroups[0];
+      kataGroups = KataGroups.fromRawKataData(rawData);
+      firstGroup = kataGroups.groups[0];
     });
     it('has the name of the kata group', () => {assert.equal(firstGroup.name, groupName)});
     it('has a katasCount', () => {assert.equal(firstGroup.katasCount, 1)});
     
-    //it('get kata group by name', function() {
-    //  kataGroups.selectGroupByName(groupName);
-    //  assert.equal(kataGroups.selectedGroup.name, groupName);
-    //});
+    it('get kata group by name', function() {
+      kataGroups.selectGroupByName(groupName);
+      assert.equal(kataGroups.selectedGroup.name, groupName);
+    });
     
   });
   it('a group without items ...', function() {
