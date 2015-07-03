3a601df2a797ba166c69f7b005a432ebe5d7e544
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 23:59:25 +0200

    Highlight hovered kata and update url in click.

--- src/components/katas.js
+++ src/components/katas.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Katas extends React.Component {
   render() {
-    const {katas=[]} = this.props.kataGroup;
+    const {katas} = this.props.kataGroup;
     return (
       <div id="list" className="pure-u-1">
         {katas.map(kata => <KataLink kata={kata} />)}
@@ -13,13 +13,15 @@ export default class Katas extends React.Component {
 
 class KataLink extends React.Component {
   render() {
-    const {name, description} = this.props.kata;
+    const {id, name, description} = this.props.kata;
+    const urlWithoutKataId = window.location.href.replace(/&kataId=\d+$/, '');
+    const newUrl = `${urlWithoutKataId}&kataId=${id}`;
     return (
       <div className="email-item pure-g">
-        <div className="pure-u-3-4">
+        <a className="pure-u-3-4" href={newUrl}>
           <h4 className="email-subject">{name}</h4>
           <p className="email-desc">{description}</p>
-        </div>
+        </a>
       </div>
     );
   }
