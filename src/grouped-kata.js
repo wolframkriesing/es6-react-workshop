08508aaf84bbb857c238fb7ab142f8c848ffc3e3
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 11:06:00 +0200

    Move grouped katas loader into own file.

---
+++ src/grouped-kata.js
@@ -0,0 +1,23 @@
+var loadFileOnServer = require('../src/server/http-get.js');
+
+function loadGroupedKata(katasUrl, onError, onSuccess) {
+  function onLoaded(err, data) {
+    var parsed;
+    try {
+      parsed = JSON.parse(data);
+    } catch (jsonParseError) {
+      onError(jsonParseError);
+    }
+    if (err) {
+      onError(err);
+    } else if (!('groups' in parsed)) {
+      onError(new Error('No groups found in the data'));
+    } else {
+      onSuccess(parsed.groups);
+    }
+  }
+
+  loadFileOnServer(katasUrl, onLoaded);
+}
+
+module.exports = loadGroupedKata;
--- test/load-json-spec.js
+++ test/load-json-spec.js
@@ -4,6 +4,8 @@ var assert = require('assert');
 var urlPrefix = 'http://katas.tddbin.com/katas/es6/language/';
 var katasUrl = urlPrefix + '__grouped__.json';
 
+var loadGroupedKata = require('../src/grouped-kata.js');
+
 describe('load ES6 kata data', function() {
   it('loaded data are as expected', function(done) {
     function onSuccess(groupedKatas) {
@@ -34,24 +36,3 @@ describe('load ES6 kata data', function() {
     });
   });
 });
-
-var loadFileOnServer = require('../src/server/http-get.js');
-function loadGroupedKata(katasUrl, onError, onSuccess) {
-  function onLoaded(err, data) {
-    var parsed;
-    try {
-      parsed = JSON.parse(data);
-    } catch (jsonParseError) {
-      onError(jsonParseError);
-    }
-    if (err) {
-      onError(err);
-    } else if (!('groups' in parsed)) {
-      onError(new Error('No groups found in the data'));
-    } else {
-      onSuccess(parsed.groups);
-    }
-  }
-
-  loadFileOnServer(katasUrl, onLoaded);
-}
