8f5b757f79d580016326a79a152126a1328b3f92
Wolfram Kriesing <wk@uxebu.com>
2015-07-04 00:49:02 +0200

    Implement selecting a kata.

--- src/katagroups.js
+++ src/katagroups.js
@@ -8,12 +8,27 @@ export default class KataGroups {
   }
   
   selectGroupByName(nameToSelect) {
-    this.groups.some((group) => {
+    this.groups.some(group => {
       if (group.name === nameToSelect) {
         this.selectedGroup = group;
         return true; // stop iterating
       }
     });
+  }
+  
+  selectKataById(kataId) {
+    this.resetSelectedKata();
+    const katasWithKataId = this.groups
+      .map(({katas}) => katas) // extract all katas, which are still arrays
+      .reduce((katas, all = []) => all.concat(katas)) // flatten the katas
+      .filter(kata => kata.id == kataId); // find the one with the id we want
+    if (katasWithKataId.length == 1) {
+      this.selectedKata = katasWithKataId[0];
+    }
+  }
+  
+  resetSelectedKata() {
+    this.selectedKata = void 0;
   }
   
 }
--- test/katagroups-spec.js
+++ test/katagroups-spec.js
@@ -4,10 +4,17 @@ import KataGroups from '../src/katagroups.js';
 describe('kata groups (data for rendering)', function() {
 
   let kataGroups;
+  const groupName = 'group one';
+  const kataName = 'jojo';
+  const secondKataId = 23;
+  const secondKataName = 'second kata';
+  const katas = [
+    {name: kataName, path: 'some', id: 42},
+    {name: secondKataName, path: 'some2', id: secondKataId}
+  ];
+  
   describe('a group', function() {
     
-    const groupName = 'group one';
-    const katas = [{name: 'jojo', path: 'some'}];
     let firstGroup;
     beforeEach(function() {
       const rawData = {[groupName]: {items: katas}};
@@ -16,17 +23,36 @@ describe('kata groups (data for rendering)', function() {
     });
     
     it('has the name of the kata group', () => {assert.equal(firstGroup.name, groupName)});
-    it('has a katasCount', () => {assert.equal(firstGroup.katasCount, 1)});
+    it('has a katasCount', () => {assert.equal(firstGroup.katasCount, 2)});
     it('has all katas', () => {assert.deepEqual(firstGroup.katas, katas)});
-    
-    it('get kata group by name', function() {
-      kataGroups.selectGroupByName(groupName);
-      assert.equal(kataGroups.selectedGroup.name, groupName);
-    });
     
   });
   it('a group without items ...', function() {
     //processRawData({groups: {'group one': {items: []}}});
   });
-});
-
+  
+  describe('select', function() {
+    describe('a kata group', function() {
+      it('by name', function() {
+        kataGroups.selectGroupByName(groupName);
+        assert.equal(kataGroups.selectedGroup.name, groupName);
+      });
+    });
+    describe('a kata', function() {
+      it('by id', function() {
+        kataGroups.selectKataById(secondKataId);
+        assert.equal(kataGroups.selectedKata.name, secondKataName);
+      });
+      describe('when ID is invalid', function() {
+        it('dont fail', function() {
+          const fn = () => { kataGroups.selectKataById(-1); };
+          assert.doesNotThrow(fn);
+        });
+        it('`selectedKata` is undefined', function() {
+          kataGroups.selectKataById(-1);
+          assert.equal(kataGroups.selectedKata, void 0);
+        });
+      });
+    });
+  });
+});  
