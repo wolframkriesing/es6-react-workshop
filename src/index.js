a7f6d7b278c36140ef6b538c752b550347af4374
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 22:59:37 +0200

    Listen to hashchange and change site accordingly.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import {KATAS_URL} from './config.js';
 import {loadRemoteFile} from './_external-deps/http-get.js';
 import React from 'react';
 import Page from './components/page.js';
+import {byUrl as selectGroupByUrl} from './selectkatagroup.js';
 
 let kataGroups;
 new RawKataData(loadRemoteFile, KATAS_URL).load(() => {}, (rawKataData) => {
   kataGroups = KataGroups.fromRawKataData(rawKataData);
   rerender(kataGroups);
+  urlChanged(window.location.href);
 });
 
-function rerender(groupedKatas) {
-  React.render(<Page kataGroups={groupedKatas}/>, document.getElementById('app'));
+function rerender(kataGroups) {
+  React.render(<Page kataGroups={kataGroups}/>, document.getElementById('app'));
 }
+
+function urlChanged(url) {
+  selectGroupByUrl(kataGroups, url);
+  rerender(kataGroups);
+}
+
+window.addEventListener('hashchange', ({newURL}) => urlChanged(newURL));
