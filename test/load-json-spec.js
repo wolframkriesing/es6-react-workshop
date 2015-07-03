6cff7773715b8d8228e15be0269860d57084a90c
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 10:23:19 +0200

    Improve test description.

--- test/load-json-spec.js
+++ test/load-json-spec.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var katasUrl = 'http://katas.tddbin.com/katas/es6/language/__grouped__.json';
 
 describe('load ES6 kata data', function() {
-  it('loading data works', function(done) {
+  it('loaded data are as expected', function(done) {
     function onLoaded(err, groupedKatas) {
       assert.equal('groups' in groupedKatas, true);
       done();
