1fccbbae6a2c4a7ca83daf227ed99979dacbf83e
Wolfram Kriesing <wk@uxebu.com>
2015-07-03 16:49:36 +0200

    Write the first react component.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import GroupedKata from './grouped-kata.js';
 import {KATAS_URL} from './config.js';
 import {loadRemoteFile} from './_external-deps/http-get.js';
 
+import React from 'react';
+
+class Page extends React.Component {
+  render() {
+    return <div>Hi I am react</div>;
+  }
+}
+
 new GroupedKata(loadRemoteFile, KATAS_URL).load(() => {}, (katas) => {
-  document.getElementById('app').innerHTML = JSON.stringify(katas);
+  React.render(<Page/>, document.getElementById('app'));
 });
