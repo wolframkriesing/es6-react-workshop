4049b2c8e36ec851072e65cac2d5f0c4c24ccb11
Wolfram Kriesing <wk@uxebu.com>
2015-07-06 21:02:33 +0200

    Made tests more explicit, to prevent refactoring bugs.

--- test/rawkatadata-spec.js
+++ test/rawkatadata-spec.js
@@ -1,7 +1,14 @@
 /* global describe, it */
 
 import assert from 'assert';
+import sinon from 'sinon';
 import RawKataData from '../src/rawkatadata.js';
+assert.notCalled = sinon.assert.notCalled;
+assert.calledOnce = sinon.assert.calledOnce;
+assert.calledOnceWith = (spy, ...param) => {
+  sinon.assert.calledOnce(spy);
+  sinon.assert.calledWith(spy, ...param);
+};
 
 function remoteFileLoaderWhichReturnsGivenData(data) {
   return (url, onLoaded) => {
@@ -13,6 +20,7 @@ function remoteFileLoaderWhichReturnsError(error) {
     onLoaded(error);
   };
 }
+function noop() {}
 
 describe('load ES6 kata data', function() {
 
@@ -28,24 +36,40 @@ describe('load ES6 kata data', function() {
   });
   describe('on error, call error callback and the error passed', function() {
 
-    it('invalid JSON', function(done) {
-      function onError(err) {
-        assert.ok(err);
-        done();
+    describe('invalid JSON', function() {
+      const error = new Error('');
+      function load(onError, onSuccess=noop) {
+        const loaderStub = remoteFileLoaderWhichReturnsError(error);
+        new RawKataData(loaderStub, 'irrelevant url').load(onError, onSuccess);
       }
-
-      const loaderStub = remoteFileLoaderWhichReturnsError(new Error(''));
-      new RawKataData(loaderStub, 'irrelevant url').load(onError);
+      it('calls onError once with the right parameter', function() {
+        let onError = sinon.stub();
+        load(onError);
+        assert.calledOnceWith(onError, error);
+      });
+      it('does NOT call onSuccess', function() {
+        let onSuccess = sinon.stub();
+        load(noop, onSuccess);
+        assert.notCalled(onSuccess);
+      });
     });
-    it('for invalid data', function(done) {
-      function onError(err) {
-        assert.ok(err);
-        done();
+    describe('for invalid data', function() {
+      function load(onError, onSuccess=noop) {
+        const invalidData = JSON.stringify({propertyGroupsMissing:{}});
+        const loaderStub = remoteFileLoaderWhichReturnsGivenData(invalidData);
+        new RawKataData(loaderStub, 'irrelevant url').load(onError, onSuccess);
       }
-
-      const invalidData = JSON.stringify({propertyGroupsMissing:{}});
-      const loaderStub = remoteFileLoaderWhichReturnsGivenData(invalidData);
-      new RawKataData(loaderStub, 'irrelevant url').load(onError);
+      it('calls onError', function() {
+        let onError = sinon.stub();
+        load(onError);
+        assert.calledOnce(onError);
+      });
+      it('does NOT call onSuccess', function() {
+        let onSuccess = sinon.stub();
+        load(noop, onSuccess);
+        assert.notCalled(onSuccess);
+      });
     });
+    
   });
 });
