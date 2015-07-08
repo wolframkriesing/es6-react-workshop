088d4671afa92fcf1796d1c96ed82aa41a6b8162
Wolfram Kriesing <wk@uxebu.com>
2015-07-08 11:56:51 +0200

    Group things nicer and ensure the component is rendered into the right DOM node.

--- test/components/app-startup-spec.js
+++ test/components/app-startup-spec.js
@@ -5,33 +5,53 @@ import {addons} from 'react/addons';
 import sinon from 'sinon';
 
 assert.called = sinon.assert.called;
+assert.calledWith = sinon.assert.calledWith;
+
+assert.renderedIntoDomNode = function(renderFunction, domNode) {
+  assert.equal(renderFunction.firstCall.args[1], domNode);
+};
 
 const TestUtils = addons.TestUtils;
 
-class ComponentDouble extends React.Component {
-  render() {
-    return <b props={this.props}></b>;
-  }
-}
 
-var rendererOutput;
 describe('app startup', function() {
 
-  function render(data={}) {
-    const shallowRenderer = TestUtils.createRenderer();
-    let app = new AppStartup(ComponentDouble, shallowRenderer.render.bind(shallowRenderer));
-    app.renderClientSide(data);
-    rendererOutput = shallowRenderer.getRenderOutput();
-  }
+  describe('the given component', function() {
 
-  it('renders the given component', function() {
-    sinon.spy(ComponentDouble.prototype, 'render');
-    render();
-    assert.called(ComponentDouble.prototype.render);
-  });
-  it('with the given data', function() {
-    let data = {my: 'data'};
-    render(data);
-    assert.deepEqual(rendererOutput.props.props, {kataGroups: data});
+    class ComponentDouble extends React.Component {
+      render() {
+        return <b props={this.props}></b>;
+      }
+    }
+
+    let rendererOutput;
+    function render(data={}) {
+      const shallowRenderer = TestUtils.createRenderer();
+      let app = new AppStartup(ComponentDouble, shallowRenderer.render.bind(shallowRenderer));
+      app.renderClientSide(data);
+      rendererOutput = shallowRenderer.getRenderOutput();
+    }
+
+    it('is rendered into given DOM node', function() {
+      const domNode = 'some ref';
+      const renderFunction = sinon.stub();
+      const ComponentStub = class extends React.Component {};
+      let app = new AppStartup(ComponentStub, renderFunction);
+      app.renderClientSide({}, domNode);
+
+      assert.renderedIntoDomNode(renderFunction, domNode);
+    });
+
+    it('is rendered', function() {
+      sinon.spy(ComponentDouble.prototype, 'render');
+      render();
+      assert.calledWith(ComponentDouble.prototype.render);
+    });
+
+    it('with the given data', function() {
+      let data = {my: 'data'};
+      render(data);
+      assert.deepEqual(rendererOutput.props.props, {kataGroups: data});
+    });
   });
 });
