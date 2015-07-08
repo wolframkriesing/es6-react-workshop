2167189de221f7810825f096b315f49500850e15
Wolfram Kriesing <wk@uxebu.com>
2015-07-08 17:47:39 +0200

    Remove propTypes, which are validated by tests anyways now.

--- src/components/page.js
+++ src/components/page.js
@@ -3,13 +3,8 @@ import React from 'react';
 import KataGroupsComponent from './katagroups';
 import KatasComponent from './katas.js';
 import Kata from './kata.js';
-import {default as KataGroupsData} from '../katagroups.js';
 
 export default class PageComponent extends React.Component {
-  static propTypes = {
-    kataGroups: React.PropTypes.instanceOf(KataGroupsData).isRequired
-  };
-
   render() {
     const {kataGroups} = this.props;
     const {selectedGroup = kataGroups.firstGroup} = kataGroups;
