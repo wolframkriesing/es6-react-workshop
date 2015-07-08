051e3d5b2323dffcf27b74092c9ca21ffa725fc1
Wolfram Kriesing <wk@uxebu.com>
2015-07-08 11:36:01 +0200

    Ensure the injected component gets rendered and it's data are as expected.

--- test/components/app-startup-spec.js
+++ test/components/app-startup-spec.js
@@ -2,23 +2,36 @@ import assert from 'assert';
 import AppStartup from '../../src/app-startup.js';
 import React from 'react';
 import {addons} from 'react/addons';
+import sinon from 'sinon';
+
+assert.called = sinon.assert.called;
 
 const TestUtils = addons.TestUtils;
 
 class ComponentDouble extends React.Component {
   render() {
-    return <div>{this.props.kataGroups}</div>;
+    return <b props={this.props}></b>;
   }
 }
 
+var rendererOutput;
 describe('app startup', function() {
-  it('renders the given component', function() {
+
+  function render(data={}) {
     const shallowRenderer = TestUtils.createRenderer();
     let app = new AppStartup(ComponentDouble, shallowRenderer);
-    let data = {my: 'data'};
     app.renderClientSide(data);
+    rendererOutput = shallowRenderer.getRenderOutput();
+  }
 
-    let output = shallowRenderer.getRenderOutput();
-    assert.deepEqual(output.props.children, data);
+  it('renders the given component', function() {
+    sinon.spy(ComponentDouble.prototype, 'render');
+    render();
+    assert.called(ComponentDouble.prototype.render);
+  });
+  it('with the given data', function() {
+    let data = {my: 'data'};
+    render(data);
+    assert.deepEqual(rendererOutput.props.props, {kataGroups: data});
   });
 });
