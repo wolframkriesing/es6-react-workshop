9b28fd46fddb1fe6bb66be3d8d4f537ddf63a8aa
Wolfram Kriesing <wk@uxebu.com>
2015-07-08 16:20:35 +0200

    Add a page component test, to ensure that Kata gets the right data. It fails!

--- test/components/page-spec.js
+++ test/components/page-spec.js
@@ -2,21 +2,20 @@ import assert from 'assert';
 import React from 'react';
 import {addons} from 'react/addons';
 import sinon from 'sinon';
-import {hasSubComponentOfType} from '../customasserts.js';
+import {hasSubComponentOfType, hasSubComponentOfTypeWithProps} from '../customasserts.js';
 
 import Page from '../../src/components/page.js';
 import Katas from '../../src/components/katas.js';
 import {default as KataGroupsData} from '../../src/katagroups.js';
 
 assert.hasSubComponentOfType = hasSubComponentOfType;
+assert.hasSubComponentOfTypeWithProps = hasSubComponentOfTypeWithProps;
 assert.called = sinon.assert.called;
 assert.calledWith = sinon.assert.calledWith;
 
 assert.renderedIntoDomNode = function(renderFunction, domNode) {
   assert.equal(renderFunction.firstCall.args[1], domNode);
 };
-
-const TestUtils = addons.TestUtils;
 
 describe('page component', function() {
 
@@ -34,10 +33,19 @@ describe('page component', function() {
 
   describe('the Katas component', function() {
     it('receives the selectedGroup from the KataGroupsData instance', function() {
-      
+      const groupName = 'kata group name';
+      const rawKataData = {[groupName]: {items: []}};
+      let kataGroups = KataGroupsData.fromRawKataData(rawKataData);
+      kataGroups.selectGroupByName(groupName);
+
+      assert.hasSubComponentOfTypeWithProps(<Page kataGroups={kataGroups}/>, Katas, {kataGroup: kataGroups.selectedGroup});
     });
     it('if no selectedGroup exists receives the first group from the KataGroupsData instance', function() {
-      
+      const rawKataData = {'first group': {items: []}, 'second group': {items: []}};
+      let kataGroups = KataGroupsData.fromRawKataData(rawKataData);
+      let selectedGroup = kataGroups.firstGroup;
+
+      assert.hasSubComponentOfTypeWithProps(<Page kataGroups={kataGroups}/>, Katas, {kataGroup: selectedGroup});
     });
   });
 });
--- test/customasserts.js
+++ test/customasserts.js
@@ -2,15 +2,32 @@ import assert from 'assert';
 import React from 'react/addons';
 const TestUtils = React.addons.TestUtils;
 
-export function hasSubComponentOfType(componentToRender, type) {
+export function hasSubComponentOfType(componentToRender, expectedComponent) {
+  var output = render(componentToRender);
+  let components = output.props.children.props.children;
+  assert.equal(components
+    .map(component => TestUtils.isElementOfType(component, expectedComponent))
+    .some(value => value)
+  , true, `Expected type to be ${expectedComponent}.`);
+}
+
+export function hasSubComponentOfTypeWithProps(componentToRender, type, expectedProps) {
+  hasSubComponentOfType(componentToRender, type);
+
+  var output = render(componentToRender);
+  const components = componentsOfType(output.props.children.props.children, type);
+  assert.deepEqual(components[0].props, expectedProps);
+}
+
+function render(componentToRender) {
   const shallowRenderer = TestUtils.createRenderer();
   shallowRenderer.render(componentToRender);
 
-  let output = shallowRenderer.getRenderOutput();
-
-  let components = output.props.children.props.children;
-  assert.equal(components
-    .map(component => TestUtils.isElementOfType(component, type))
-    .some(value => value)
-  , true, `Expected type to be ${type}.`);
+  return shallowRenderer.getRenderOutput();
 }
+
+function componentsOfType(components, filterForComponent) {
+  return components
+      .filter(component => TestUtils.isElementOfType(component, filterForComponent));
+}
+
