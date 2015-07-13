e75ce577d5c916391a1b12d3ccdcc1445a995b3f
Wolfram Kriesing <wk@uxebu.com>
2015-07-13 22:45:27 +0200

    Use external module for testing stuff in the DOM nodes.

--- test/components/katas-spec.js
+++ test/components/katas-spec.js
@@ -1,29 +1,18 @@
 import assert from 'assert';
 import React from 'react/addons';
 const TestUtils = React.addons.TestUtils;
-import {rendersDomNodeWithAttrAndValue} from 'react-components-asserts';
+import {
+  rendersDomNodeWithAttrAndValue,
+  rendersDomNodeWithTextContent
+} from 'react-components-asserts';
 
 assert.rendersDomNodeWithAttrAndValue = rendersDomNodeWithAttrAndValue;
+assert.rendersDomNodeWithTextContent = rendersDomNodeWithTextContent;
 
 import KatasComponent from '../../src/components/katas.js';
 import KataGroups from '../../src/katagroups.js';
 
 describe('KatasComponent', function() {
-
-  assert.rendersDomNodeAttributeWithValue = function(component, attribute, expectedValue) {
-    let output = render(component);
-    let firstKata = render(output.props.children[0]).props.children.props;
-    assert.equal(firstKata[attribute], expectedValue);
-  };
-  assert.rendersDomNodeWithInnerText = function(componentToRender, text) {
-    let output = render(render(componentToRender).props.children[0]);
-    let matches = output.props.children.props.children
-      .map(({props: {children}}) => children)
-      .filter(content => content === text)
-    ;
-    var message = `Expected \`${componentToRender.type.name}\` to render a DOM node with innerText \`${text}\``;
-    assert.equal(matches.length > 0, true, message);
-  };
 
   let component;
   let kataGroup;
@@ -48,13 +37,15 @@ describe('KatasComponent', function() {
     let renderedKatas = output.props.children;
     assert.equal(renderedKatas.length, 1);
   });
-  it('render the `name`', function() {
-    assert.rendersDomNodeWithInnerText(component, kataName);
-  });
-  it('render the `description`', function() {
-    assert.rendersDomNodeWithInnerText(component, description);
-  });
-  it('render the URL properly', function() {
-    assert.rendersDomNodeWithAttrAndValue(component, 'href', '???&kataId=1');
+  describe('renders the properties', function() {
+    it('the `name`', function() {
+      assert.rendersDomNodeWithTextContent(component, kataName);
+    });
+    it('the `description`', function() {
+      assert.rendersDomNodeWithTextContent(component, description);
+    });
+    it('the URL properly', function() {
+      assert.rendersDomNodeWithAttrAndValue(component, 'href', '???&kataId=1');
+    });
   });
 });
