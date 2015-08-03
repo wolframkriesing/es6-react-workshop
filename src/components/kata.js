c6e9b266405dd3c4cdce3da9e31216d185284afd
Wolfram Kriesing <wk@uxebu.com>
2015-08-03 19:13:20 +0200

    Render all the kata text properly.

--- src/components/kata.js
+++ src/components/kata.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 export default class KataComponent extends React.Component {
   render() {
+    const {kata} = this.props;
     return (
       <div id="main" className="pure-u-1">
         <div className="email-content">
           <div className="email-content-header pure-g">
             <div className="pure-u-1-2">
-              <h1 className="email-content-title">Template strings - basics</h1>
+              <h1 className="email-content-title">{kata.name}</h1>
 
-              <p className="email-content-subtitle">
-                A template string, is wrapped in backticks.
-              </p>
+              <p className="email-content-subtitle">{kata.description}</p>
             </div>
 
             <div className="email-content-controls pure-u-1-2">
---
+++ test/components/kata-spec.js
@@ -0,0 +1,25 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import KataComponent from '../../src/components/kata.js';
+import {
+  rendersDomNodeWithAttrAndValue,
+  rendersDomNodeWithTextContent
+} from 'react-components-asserts';
+
+describe('katas component', function() {
+
+  const kata = {
+    name: 'kata name',
+    description: 'kata desc'
+  };
+
+  it('renders the name', function() {
+    let comp = <KataComponent kata={kata} />;
+    rendersDomNodeWithTextContent(comp, kata.name);
+  });
+
+  it('renders the description', function() {
+    let comp = <KataComponent kata={kata} />;
+    rendersDomNodeWithTextContent(comp, kata.description);
+  });
+  
+});
