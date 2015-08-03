744e01c703adfd145a812302c5c86d18512ba0ef
Wolfram Kriesing <wk@uxebu.com>
2015-08-03 16:51:35 +0200

    Ensure passing on the appUrl.

--- test/components/page-spec.js
+++ test/components/page-spec.js
@@ -35,21 +35,22 @@ describe('page component', function() {
   });
 
   describe('the Katas component', function() {
+    const appUrlDouble = {};
     it('receives the selectedGroup from the KataGroupsData instance', function() {
       const groupName = 'kata group name';
       const rawKataData = {[groupName]: {items: []}};
       let kataGroups = KataGroups.fromRawKataData(rawKataData);
       kataGroups.selectGroupByName(groupName);
 
-      var expectedProps = {kataGroup: kataGroups.selectedGroup};
-      assert.hasSubComponentOfTypeWithProps(<PageComponent kataGroups={kataGroups}/>, KatasComponent, expectedProps);
+      var expectedProps = {kataGroup: kataGroups.selectedGroup, appUrl: appUrlDouble};
+      assert.hasSubComponentOfTypeWithProps(<PageComponent kataGroups={kataGroups} appUrl={appUrlDouble} />, KatasComponent, expectedProps);
     });
     it('if no selectedGroup exists receives the firstGroup from the KataGroupsData instance', function() {
       const rawKataData = {'first group': {items: []}, 'second group': {items: []}};
       let kataGroups = KataGroups.fromRawKataData(rawKataData);
 
-      var expectedProps = {kataGroup: kataGroups.firstGroup};
-      assert.hasSubComponentOfTypeWithProps(<PageComponent kataGroups={kataGroups}/>, KatasComponent, expectedProps);
+      var expectedProps = {kataGroup: kataGroups.firstGroup, appUrl: appUrlDouble};
+      assert.hasSubComponentOfTypeWithProps(<PageComponent kataGroups={kataGroups} appUrl={appUrlDouble}/>, KatasComponent, expectedProps);
     });
   });
 });
