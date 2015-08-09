f3aed5c0288738c7f748bc39596c12a9e38e520f
Wolfram Kriesing <wk@uxebu.com>
2015-08-09 17:08:05 +0200

    Playing with the UI.

--- src/components/kata.js
+++ src/components/kata.js
@@ -10,7 +10,7 @@ export default class KataComponent extends React.Component {
         <div className="email-content">
           <div className="email-content-header pure-g">
             <div className="pure-u-1-5">
-              <a className="primary-button pure-button" target="_blank" href={outgoingUrl.toKataOnTddbin(kata)}>Open in TDDbin</a>
+              <a className="primary-button pure-button" target="_blank" href={outgoingUrl.toKataOnTddbin(kata)}>Solve this kata</a>
             </div>
             <div className="pure-u-4-5">
               <h1 className="email-content-title"><a href={appUrl.buildUrlForKata(kata)}>#{kata.id}</a> {group.name}: {kata.name}</h1>
@@ -20,6 +20,17 @@ export default class KataComponent extends React.Component {
           </div>
 
           <div className="email-content-body">
+            <h2>The following katas are recommended to do before this kata</h2>
+            <ul>
+              <li><a href={appUrl.buildUrlForKata(kata.dependsOnKatas)}>#8 Block scope: `const` declaration</a></li>
+              <li><a href={appUrl.buildUrlForKata(kata.dependsOnKatas)}>#5 Arrow functions: basics</a></li>
+            </ul>
+
+            <h2>More in depth links</h2>
+            <ul>
+              <li><a href="">Moziila MDN</a></li>
+            </ul>
+
             <h2>Solving this kata you can learn</h2>
             <ul>
               <li>one thing</li>
